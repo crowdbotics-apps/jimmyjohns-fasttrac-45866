19625ed9e0eb6687df0d57512024d73dbac97be9
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:42:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jimmyjohns-fasttrac-45866",
+  url: "https://jimmyjohns-fasttrac-45866.botics.co",
+  api: "https://jimmyjohns-fasttrac-45866.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
